49282ace4568102763e77fc3bd8bd9efe1e7a684
maricotab <161454065+maricotab@users.noreply.github.com>
2025-01-05 14:56:19 -0300

    Update Pedido.ts

--- src/Pedido.ts
+++ src/Pedido.ts
@@ -2,7 +2,15 @@ import { Prato } from "./Prato";
 export class Pedido {
     itens: Prato [] = [];
 
-    constructor(){}
+    constructor(itens: Prato []){
+        this.itens = itens;
+    }
 
-    calcular_conta():void{}
+    calcular_conta():void{
+        let total = 0 
+        this.itens.forEach (prato => {
+            total += prato.preco; 
+        });
+        return total;
+    }
 }
