ac9063f757f48a79d40789be56c7f3d43aab4475
gabii1210 <161453459+gabii1210@users.noreply.github.com>
2025-01-06 10:22:51 -0300

    Update Prato.ts
    c lsjh

--- src/Prato.ts
+++ src/Prato.ts
@@ -1,12 +1,10 @@
 export class Prato {
     private id_Prato: int;
-    private quantidade:number
     private nome:string
     private preco:number
 
-    constructor(id_Prato: int, quantidade: number, nome: string, preco: number){
+    constructor(id_Prato: int, nome: string, preco: number){
         this.id_Prato= int;
-        this.quantidade = quantidade;
         this.nome = nome; 
         this.preco= preco;
     }  
