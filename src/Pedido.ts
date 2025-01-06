9a80ed22574edc93f92861da2e7c553d48d93a93
gabii1210 <161453459+gabii1210@users.noreply.github.com>
2025-01-06 10:21:27 -0300

    Update Pedido.ts
    adicionei quantidade

--- src/Pedido.ts
+++ src/Pedido.ts
@@ -1,17 +1,19 @@
 import { Prato } from "./Prato";
 export class Pedido {
     id_Pedido = int,
+    quantidade= number;
     itens: Prato [] = [];
 
-    constructor(id_Pedido: int, itens: Prato []){
+    constructor(id_Pedido: int, quantidade: number, itens: Prato []){
         this.id_Pedido = int;
+        this.quantidade = number:
         this.itens = itens;
     }
 
     calcular_conta():void{
         let total = 0 
         this.itens.forEach (prato => {
-            total += prato.preco; 
+            total += (prato.preco * pedido.quantidade); 
         });
         return total;
     }
