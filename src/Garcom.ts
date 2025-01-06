6f2b63514728a8b7d058c2335b80461391a8f8d4
gabii1210 <161453459+gabii1210@users.noreply.github.com>
2025-01-06 10:06:09 -0300

    Update Garcom.ts
    id_Garcom

--- src/Garcom.ts
+++ src/Garcom.ts
@@ -2,9 +2,10 @@ export class Garcom{
     private nome:string
     private disponibilidade:boolean=true
     private taxa:number
+    private id_Garcom = int
 
-    constructor(nome: string, disponibilidade: boolean, taxa: number){ 
-        this id_cliente= int
+    constructor(id_Garcom:int, nome: string, disponibilidade: boolean, taxa: number){ 
+        this.id_Garcom= int
         this.nome = nome;
         this.disponibilidade = disponibilidade;
         this.taxa = taxa;
