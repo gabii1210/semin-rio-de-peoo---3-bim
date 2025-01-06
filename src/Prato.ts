741b7382cb0f3c26cd58dfe68d028fbb9c5eb771
gabii1210 <161453459+gabii1210@users.noreply.github.com>
2025-01-06 10:13:03 -0300

    Update Prato.ts
    gfvfhj

--- src/Prato.ts
+++ src/Prato.ts
@@ -1,9 +1,11 @@
 export class Prato {
+    private id_Prato: int;
     private quantidade:number
     private nome:string
     private preco:number
 
-    constructor(quantidade: number, nome: string, preco: number){
+    constructor(id_Prato: int, quantidade: number, nome: string, preco: number){
+        this.id_Prato= int;
         this.quantidade = quantidade;
         this.nome = nome; 
         this.preco= preco;
